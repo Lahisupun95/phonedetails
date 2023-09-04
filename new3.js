7a5187a362b1892a2dd0fdbd690f213119ab056c
Lahisupun95 <lahirusupun95@gmail.com>
2023-09-04 21:49:24 +0530

    try this

---
+++ new3.js
@@ -0,0 +1,6 @@
+$('.supun').click(function() {$("h1").html("tile is change")});
+$(document).ready(function(){
+    $("#p1").mousedown(function(){
+      alert("Mouse down over p1!");
+    });
+  });
